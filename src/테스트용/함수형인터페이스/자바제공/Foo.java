635d05a50da80f68bb3bfd14eae22bd448c14364
NamYeju <dpwn0112@naver.com>
2022-03-29 23:15:38 +0900

    0329

---
+++ src/테스트용/함수형인터페이스/자바제공/Foo.java
@@ -0,0 +1,17 @@
+package 테스트용.함수형인터페이스.자바제공;
+
+import java.util.function.Function;
+
+public class Foo implements Function<Integer, Integer> {
+
+
+    @Override
+    public Integer apply(Integer integer) {
+        return integer + 10;
+    }
+
+    public static void main(String[] args) {
+        Foo foo  = new Foo();
+        System.out.println(foo.apply(10));
+    }
+}
